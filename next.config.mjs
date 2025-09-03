385eb4ea714749a7357eeddef32fbae889bca074
Newalfox <52476035+Alane38@users.noreply.github.com>
2025-09-03 11:56:21 +0200

    Android Compiler setup with Java 21

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isMobile = process.env.NEXT_PUBLIC_IS_MOBILE === 'true';
 const nextConfig = {
+      ...(isMobile ? {output: 'export'} : {}),
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
